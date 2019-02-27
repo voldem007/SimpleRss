5cc3876481cd2d314781d3f94ccc09b2e09928d8
Voldem <vladimir_koptev@navitaire.com>
2019-02-27 15:38:17 +0300

    fix c/r issues

--- SimpleRss/SimpleRss/Services/DataService.swift
+++ SimpleRss/SimpleRss/Services/DataService.swift
@@ -13,7 +13,7 @@ class DataService {
     
     lazy var updateContext: NSManagedObjectContext = {
         let _updateContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
-        _updateContext.parent = viewContext
+        _updateContext.persistentStoreCoordinator = appDelegate.persistentContainer.persistentStoreCoordinator
         return _updateContext
     }()
     
@@ -21,34 +21,32 @@ class DataService {
         return UIApplication.shared.delegate as! AppDelegate
     }()
     
-    private var viewContext: NSManagedObjectContext {
-        return appDelegate.persistentContainer.viewContext
-    }
-    
     func getTopics() -> [TopicModel]? {
         
-        let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Topic")
+        let fetch: NSFetchRequest<Topic> = Topic.fetchRequest()
         let topics = try? updateContext.fetch(fetch)
         
         return topics?.map { topic in
-            let _topic = topic as? Topic
-            return TopicModel(title: _topic?.title ?? "", picUrl: _topic?.picLink ?? "", feedUrl: _topic?.feedUrl ?? "")
+            return TopicModel(title: topic.title ?? "", picUrl: topic.picLink ?? "", feedUrl: topic.feedUrl ?? "")
         }
     }
     
     func getFeed(by feedUrl: String) -> [FeedModel]? {
         
-        let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Topic")
-        let predicate = NSPredicate(format: "feedUrl = %@", argumentArray : [feedUrl])
-        fetch.predicate = predicate
-        let topic = try? updateContext.fetch(fetch).first
-        let _topic = topic as? Topic
-        
-        let feedModels: [FeedModel]? = _topic?.feed?.map { element in
-            let _element = element as? Feed
-            return FeedModel(title: _element?.title, pubDate: _element?.pubDate, picLink: _element?.picLink, description: _element?.text)
+        var feedModels: [FeedModel]?
+        updateContext.performAndWait {
+            
+            let fetch: NSFetchRequest<Topic> = Topic.fetchRequest()
+            let predicate = NSPredicate(format: "feedUrl = %@", argumentArray : [feedUrl])
+            fetch.predicate = predicate
+            let topic = try? fetch.execute().first ?? Topic()
+            
+            feedModels = topic?.feed?.map { element in
+                let _element = element as? Feed
+                return FeedModel(title: _element?.title, pubDate: _element?.pubDate, picLink: _element?.picLink, description: _element?.text)
+            }
+
         }
-        
         return feedModels?.count == 0 ? nil : feedModels
     }
     
@@ -56,34 +54,39 @@ class DataService {
         
         deleteFeed(by: url)
         
-        let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Topic")
-        let predicate = NSPredicate(format: "feedUrl = %@", argumentArray: [url])
-        fetch.predicate = predicate
-        let topic = try? updateContext.fetch(fetch).first
-        let _topic = topic as? Topic
+        updateContext.perform { [weak self] in
+            guard let self = self else { return }
         
-        feedList.forEach({ feedModel in            
-            let feed = Feed(context: updateContext)
-            feed.text = feedModel.description
-            feed.pubDate = feedModel.pubDate
-            feed.picLink = feedModel.picLink
-            feed.title = feedModel.title
-            _topic?.addToFeed(feed)
-        })
-        
-        try? updateContext.save()
-        try? updateContext.parent?.save()
+            let fetch: NSFetchRequest<Topic> = Topic.fetchRequest()
+            let predicate = NSPredicate(format: "feedUrl = %@", argumentArray: [url])
+            fetch.predicate = predicate
+            let topic = try? fetch.execute().first
+            let _topic = topic as? Topic
+            
+            feedList.forEach({ feedModel in
+                let feed = Feed(context: self.updateContext)
+                feed.text = feedModel.description
+                feed.pubDate = feedModel.pubDate
+                feed.picLink = feedModel.picLink
+                feed.title = feedModel.title
+                _topic?.addToFeed(feed)
+            })
+            
+            try? self.updateContext.save()
+        }
     }
     
     func deleteFeed(by feedUrl: String) {
         
-        let fetchForDelete = NSFetchRequest<NSFetchRequestResult>(entityName: "Feed")
-        fetchForDelete.predicate = NSPredicate(format: "topic.feedUrl = %@", argumentArray : [feedUrl])
-        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchForDelete)
+        updateContext.performAndWait {
         
-        _ = try? updateContext.execute(deleteRequest)
-        
-        updateContext.reset()
-        updateContext.parent?.reset()
+            let fetchForDelete = NSFetchRequest<NSFetchRequestResult>(entityName: "Feed")
+            fetchForDelete.predicate = NSPredicate(format: "topic.feedUrl = %@", argumentArray : [feedUrl])
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchForDelete)
+            
+            _ = try? updateContext.execute(deleteRequest)
+            
+            updateContext.reset()
+        }
     }
 }
