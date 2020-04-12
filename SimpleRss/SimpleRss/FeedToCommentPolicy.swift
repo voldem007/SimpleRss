3aa76dc940a8de0177ddac54b6e7c13a70433f06
VKoptev <>
2020-04-12 16:14:35 +0300

    Avoid empty comment during migration

--- SimpleRss/SimpleRss/FeedToCommentPolicy.swift
+++ SimpleRss/SimpleRss/FeedToCommentPolicy.swift
@@ -2,13 +2,13 @@ import CoreData
 
 class FeedToCommentPolicy: NSEntityMigrationPolicy {
     override func createDestinationInstances(forSource sInstance: NSManagedObject, in mapping: NSEntityMapping, manager: NSMigrationManager) throws {
- 
-        let dInstance = NSEntityDescription.insertNewObject(forEntityName: mapping.destinationEntityName!, into: manager.destinationContext)
         
         let comment = sInstance.primitiveValue(forKey: "comment") as? String
         let rating = sInstance.primitiveValue(forKey: "rating") as? Double
         
         if rating != nil || comment != nil {
+            let dInstance = NSEntityDescription.insertNewObject(forEntityName: mapping.destinationEntityName!, into: manager.destinationContext)
+            
             dInstance.setValue(UUID().uuidString, forKey: "id")
             dInstance.setValue(comment, forKey: "comment")
             dInstance.setValue(rating, forKey: "rating")
